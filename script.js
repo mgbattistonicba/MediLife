60246c63bee651fef6f847fe71be2819aac596d3
marcos battistoni <mgbattistoni@gmail.com>
2023-07-12 23:43:10 -0300

    cambio gral

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function unselect(){
-    unselect.checkend=false;
+function limpiarRadioButton(){
+    document.querySelectorAll('[name=Tipo]').forEach((x) => x.checked=false);
   }
+
+let radios = document.querySelectorAll("[type='radio']");
+   radios.forEach((x)=>{
+     x.dataset.val = x.checked;
+     x.addEventListener('click',(e)=>{
+       let element = e.target;
+       if(element.dataset.val == 'false'){
+         element.dataset.val = 'true';
+         element.checked = true;
+       }else{
+         element.dataset.val = 'false';
+         element.checked = false;
+       }
+     },true);
+   });
